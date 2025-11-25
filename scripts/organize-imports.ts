53bb3a96519c8849554c0d6c2bff18382079fa36
Rafał <xbl@gazeta.pl>
2025-11-25 19:07:21 +0100

    chore: add organize-imports script and update lint-staged configuration
    - Introduced a new script to organize import statements in TypeScript files for better readability and consistency.
    - Updated the lint-staged configuration to use 'tsx' for running the organize-imports script.

---
+++ scripts/organize-imports.ts
@@ -0,0 +1,84 @@
+import { ImportDeclaration, Project } from "ts-morph";
+
+const files = process.argv.slice(2);
+if (files.length === 0) process.exit(0);
+
+const project = new Project({
+  skipAddingFilesFromTsConfig: true,
+});
+
+project.addSourceFilesAtPaths(files);
+
+for (const file of project.getSourceFiles()) {
+  const imports = file.getImportDeclarations();
+  if (imports.length === 0) continue;
+
+  const minIndex = Math.min(
+    ...imports.map((importDecl: ImportDeclaration) =>
+      importDecl.getChildIndex()
+    )
+  );
+
+  const sideEffectImports: ImportDeclaration[] = [];
+  const namedImports: ImportDeclaration[] = [];
+  const mixedImports: ImportDeclaration[] = [];
+  const defaultImports: ImportDeclaration[] = [];
+
+  for (const imp of imports) {
+    const defaultImport = imp.getDefaultImport();
+    const clause = imp.getImportClause();
+
+    // Side-effect: import 'foo'; (no clause)
+    if (!clause) {
+      sideEffectImports.push(imp);
+      continue;
+    }
+
+    const namedBindings = clause.getNamedBindings(); // NamedImports or NamespaceImport
+    const isNamespace =
+      namedBindings && namedBindings.getKindName() === "NamespaceImport";
+
+    const hasNamedStructure =
+      namedBindings && namedBindings.getKindName() === "NamedImports";
+
+    if (isNamespace) {
+      // Namespace: import * as N from 'a' -> No {}. Group with Default.
+      defaultImports.push(imp);
+    } else if (hasNamedStructure) {
+      if (defaultImport) {
+        // Named + Default -> Mixed
+        mixedImports.push(imp);
+      } else {
+        // Named only
+        namedImports.push(imp);
+      }
+    } else {
+      // No NamedStructure (so Default only)
+      defaultImports.push(imp);
+    }
+  }
+
+  const sortFn = (a: ImportDeclaration, b: ImportDeclaration) => {
+    return a
+      .getModuleSpecifierValue()
+      .localeCompare(b.getModuleSpecifierValue());
+  };
+
+  sideEffectImports.sort(sortFn);
+  namedImports.sort(sortFn);
+  mixedImports.sort(sortFn);
+  defaultImports.sort(sortFn);
+
+  const sortedImports: ImportDeclaration[] = [
+    ...sideEffectImports,
+    ...namedImports,
+    ...mixedImports,
+    ...defaultImports,
+  ];
+
+  sortedImports.forEach((imp: ImportDeclaration, index: number) => {
+    imp.setOrder(minIndex + index);
+  });
+
+  file.saveSync();
+}
